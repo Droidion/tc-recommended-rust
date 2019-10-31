e9def73aa97d510ada17b5f9bef0eadc5405cdf9
Denis Rodionov <zunh@protonmail.com>
2019-11-01 00:49:32 +0200

    Load from CSV

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,46 @@
 extern crate askama;
 
+use std::error::Error;
+use csv::ReaderBuilder;
+use std::fs;
+
 mod web;
 
+#[derive(Debug)]
+struct TopListItem {
+    composer_name: String,
+    composer_slug: String,
+    work: String,
+    list_name: String,
+    list_slug: String,
+}
+
+fn load_top_list_from_csv() -> Result<Vec<TopListItem>, Box<dyn Error>> {
+    let mut top_list_items: Vec<TopListItem> = Vec::new();
+    for entry in fs::read_dir("csv")? {
+        let dir_entry = entry.unwrap();
+        let mut file_name = dir_entry.file_name().into_string().unwrap();
+        file_name.truncate(file_name.len() - 4);
+        let mut rdr = ReaderBuilder::new()
+            .has_headers(false)
+            .delimiter(b'|')
+            .from_path(&dir_entry.path())?;
+        for result in rdr.records() {
+            let record = result?;
+            top_list_items.push(TopListItem {
+                composer_name: record[0].parse()?,
+                composer_slug: String::from("asdasd"),
+                work: record[1].parse()?,
+                list_name: file_name.clone(),
+                list_slug: String::from("asdasd"),
+            });
+        }
+    }
+    Ok(top_list_items)
+}
+
 pub fn run() {
-    web::start_server();
+    let all_list_data = load_top_list_from_csv();
+    println!("{:?}", res);
+    // web::start_server();
 }
