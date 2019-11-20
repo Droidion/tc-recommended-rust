5c8916263caa21b6e5075ecfe98e674b577d9e1a
Denis Rodionov <zunh@protonmail.com>
2019-11-21 00:53:44 +0200

    Refactor and comments

--- src/lib.rs
+++ src/lib.rs
@@ -11,27 +11,39 @@ use std::fs;
 
 mod web;
 
+/// Main source of data, works from all lists by all composers, in flat form
 #[derive(Debug)]
 pub struct TopListItem {
+    // Composer`s human readable name
     pub composer_name: String,
+    // Composer`s slug for using in URL
     pub composer_slug: String,
+    // Name of a single work
     pub work: String,
+    // List`s human readable name
     pub list_name: String,
+    // List`s slug for using in URL
     pub list_slug: String,
+    // Work`s position in its list
     pub position: usize,
 }
 
+/// Convert URL slug to human readable name: "foo-bar" -> "Foo Bar"
+/// We substitute data from our main dictionary in app state, not converting strings directly
 fn slug_to_name(list: &Vec<(String, String, usize)>, slug: String) -> String {
+    // Only substitute if slug is found
     match list.iter().find(|item| item.1 == slug) {
         Some(item) => item.0.clone(),
         None => String::from(""),
     }
 }
 
+/// Convert human readable name to URL slug: "Foo Bar" -> "foo-bar"
 fn name_to_slug(slug: String) -> String {
     to_kebab_case(slug.as_str())
 }
 
+/// Get all works by all composers from a single list
 fn filter_by_list_name(
     items: &'static Vec<TopListItem>,
     name: &String,
@@ -42,36 +54,45 @@ fn filter_by_list_name(
         .collect()
 }
 
+/// Get all works by a single composer, grouped by list
 fn filter_by_composer_name(
     items: &'static Vec<TopListItem>,
     name: &String,
 ) -> Vec<(&'static str, &'static str, Vec<&'static TopListItem>)> {
     let mut res: Vec<(&str, &str, Vec<&'static TopListItem>)> = vec![];
+    // Get flat data
     let filtered: Vec<&'static TopListItem> = items
         .iter()
         .filter(|item| item.composer_name.to_lowercase() == name.to_lowercase())
         .collect();
+    // Group by list
     for item in filtered {
         match res.iter().position(|x| x.0 == item.list_name) {
             Some(x) => res[x].2.push(item),
             None => res.push((item.list_name.as_ref(), item.list_slug.as_ref(), vec![item])),
         }
     }
+    // Sort by list name alphabetically
     res.sort_by(|a, b| a.0.cmp(&b.0));
     res
 }
 
-fn load_top_list_from_csv() -> Vec<TopListItem> {
+/// Load all top lists from CSV files
+fn top_list_from_csv() -> Vec<TopListItem> {
     let mut all_lists: Vec<TopListItem> = Vec::new();
+    // Read all files in the directory with CSV files
     for entry in fs::read_dir("csv").unwrap() {
         let dir_entry = entry.unwrap();
+        // We want file names to become lists names
         let mut file_name = dir_entry.file_name().into_string().unwrap();
+        // Remove ".csv" from file names
         file_name.truncate(file_name.len() - 4);
         let mut rdr = ReaderBuilder::new()
             .has_headers(false)
             .delimiter(b'|')
             .from_path(&dir_entry.path())
             .unwrap();
+        // Read all string from the CSV file
         for (i, result) in rdr.records().enumerate() {
             let record = result.unwrap();
             all_lists.push(TopListItem {
@@ -87,7 +108,8 @@ fn load_top_list_from_csv() -> Vec<TopListItem> {
     all_lists
 }
 
-fn get_menu(items: &'static Vec<TopListItem>) -> Vec<(String, String, usize)> {
+/// Build dynamic site menu based on the top lists parsed from csv
+fn menu(items: &'static Vec<TopListItem>) -> Vec<(String, String, usize)> {
     let mut menu: Vec<(String, String)> = items
         .iter()
         .map(|el| (el.list_name.clone(), el.list_slug.clone()))
@@ -103,16 +125,19 @@ fn get_menu(items: &'static Vec<TopListItem>) -> Vec<(String, String, usize)> {
         .collect()
 }
 
-fn get_top_composers(
+/// Build the list of best composers based on the top lists parsed from csv
+fn top_composers(
     items: &'static Vec<TopListItem>,
     menu: &'static Vec<(String, String, usize)>,
 ) -> Vec<(String, String, usize)> {
+    // Extract list of composers
     let mut composers: Vec<(String, String)> = items
         .iter()
         .map(|el| (el.composer_name.clone(), el.composer_slug.clone()))
         .collect();
     composers.sort();
     composers.dedup();
+    // Add scores to the list of composers
     let mut composers_with_scores: Vec<(String, String, usize)> = composers
         .iter()
         .map(|el| {
@@ -136,14 +161,19 @@ fn get_top_composers(
             (el.0.clone(), el.1.clone(), score)
         })
         .collect();
+    // Sort composers by calculated score
     composers_with_scores.sort_by(|a, b| b.2.cmp(&a.2));
     composers_with_scores
 }
 
+// Create app store
 lazy_static! {
-    static ref LISTS: Vec<TopListItem> = load_top_list_from_csv();
-    static ref MENU: Vec<(String, String, usize)> = get_menu(&LISTS);
-    static ref COMPOSERS: Vec<(String, String, usize)> = get_top_composers(&LISTS, &MENU);
+    // All top lists in flat form
+    static ref LISTS: Vec<TopListItem> = top_list_from_csv();
+    // Site dynamic menu of top lists
+    static ref MENU: Vec<(String, String, usize)> = menu(&LISTS);
+    // List of best composers sorted by their calculated score
+    static ref COMPOSERS: Vec<(String, String, usize)> = top_composers(&LISTS, &MENU);
 }
 
 pub fn run() {
--- src/web.rs
+++ src/web.rs
@@ -3,39 +3,56 @@ use actix_files as fs;
 use actix_web::{middleware, web, App, HttpResponse, HttpServer, Result};
 use askama::Template;
 
+/// Payload for rendering sorted list of best composers page
 #[derive(Template)]
 #[template(path = "top-composers.html")]
 struct TopComposersTemplate<'a> {
+    // Page title
     title: &'a str,
+    // Current page slug, for highlighting selected menu item
     selected_slug: &'a str,
+    // Sorted list of best composers
     items: &'a Vec<(String, String, usize)>,
+    // Dynamic part of site menu
     menu: &'a Vec<(String, String, usize)>,
 }
 
+/// Payload for rendering a composer page
 #[derive(Template)]
 #[template(path = "composer.html")]
 struct ComposerTemplate<'a> {
-    composerslug: &'a str,
+    // Human readable composer name
+    composer_name: &'a str,
+    // Current page slug, for highlighting selected menu item
     selected_slug: &'a str,
+    // Works of a single composer grouped by lists
     items: &'a Vec<(&'a str, &'a str, Vec<&'static TopListItem>)>,
+    // Dynamic part of site menu
     menu: &'a Vec<(String, String, usize)>,
 }
 
+/// Payload for rendering a single list page
 #[derive(Template)]
 #[template(path = "list.html")]
 struct ListTemplate<'a> {
-    listslug: &'a str,
+    // Human readable list name
+    list_name: &'a str,
+    // Current page slug, for highlighting selected menu item
     selected_slug: &'a str,
+    // Works of single list
     items: &'a Vec<&'a TopListItem>,
+    // Dynamic part of site menu
     menu: &'a Vec<(String, String, usize)>,
 }
 
+/// Helper for rendering a page with some data
 fn render_page<T: askama::Template>(s: &T) -> Result<HttpResponse> {
     Ok(HttpResponse::Ok()
         .content_type("text/html")
         .body(&s.render().unwrap()))
 }
 
+/// Render sorted list of best composers
 fn top_composers() -> Result<HttpResponse> {
     render_page(&TopComposersTemplate {
         title: "Top composers",
@@ -45,28 +62,31 @@ fn top_composers() -> Result<HttpResponse> {
     })
 }
 
+/// Render all works by a single composer grouped by lists
 fn composer(info: web::Path<String>) -> Result<HttpResponse> {
     let name = &crate::slug_to_name(&crate::COMPOSERS, info.to_string());
     let items = crate::filter_by_composer_name(&crate::LISTS, name);
     render_page(&ComposerTemplate {
-        composerslug: name.as_str(),
+        composer_name: name.as_str(),
         selected_slug: "",
         items: &items,
         menu: &crate::MENU,
     })
 }
 
+/// Render page with works from a single list
 fn list(info: web::Path<String>) -> Result<HttpResponse> {
     let name = &crate::slug_to_name(&crate::MENU, info.to_string());
     let items = crate::filter_by_list_name(&crate::LISTS, name);
     render_page(&ListTemplate {
-        listslug: name.as_str(),
+        list_name: name.as_str(),
         selected_slug: info.as_str(),
         items: &items,
         menu: &crate::MENU,
     })
 }
 
+/// Start web server
 pub fn start_server() {
     HttpServer::new(move || {
         App::new()
